fca01c7994cffd4567c5d24169182aa0867e8cff
AndrewSB <asbreckenridge@me.com>
2014-11-10 13:10:49 -0600

    start adding the media player

--- PizzaFM/ViewController.swift
+++ PizzaFM/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class ViewController: UIViewController {
-
+    
+    let player: MPMoviePlayerController = MPMoviePlayerController()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        play()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func play() {
+        player.contentURL = NSURL(fileURLWithPath: "http://dir.xiph.org/listen/1042227/listen.m3u")
+        player.movieSourceType = .Streaming
+        player.prepareToPlay()
+        self.view.addSubview(player.view)
+        player.play()
+    }
 
 }
 
