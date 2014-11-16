043ad34bf0741b4a4c4247b0988305b7438b989e
AndrewSB <asbreckenridge@me.com>
2014-11-15 20:47:23 -0800

    add persistant stuff

--- PizzaFM/AppDelegate.swift
+++ PizzaFM/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        
+        let session = AVAudioSession.sharedInstance()
+        let sessionCategory = kAudioSessionCategory_MediaPlayback;
+        var setCategoryError: NSError?
+        
+        session.setCategory(AVAudioSessionCategoryPlayback, error: &setCategoryError)
+        
         return true
     }
 
